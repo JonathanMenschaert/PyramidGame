be1215282f140150d5edd670bb47a68cdddeff64
Jonathan Menschaert <jonathan.658@telenet.be>
2024-08-26 13:46:33 +0200

    Setting up editor settings and labyrinth module

---
+++ PyramidGame/Source/PyramidGame/Private/CompileRequirementClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CompileRequirementClass.h"
+
+CompileRequirementClass::CompileRequirementClass()
+{
+}
+
+CompileRequirementClass::~CompileRequirementClass()
+{
+}
